afbaf9c1be874ec10bac3ccfabea1e7cc2ccf8d3
qkrwns1478 <qkrwns1478@gmail.com>
2026-05-05 19:34:33 +0900

    chore: FeignClient로 전환

---
+++ serviceb/src/main/java/com/example/serviceb/client/ServiceAClient.java
@@ -0,0 +1,10 @@
+package com.example.serviceb.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "servicea")
+public interface ServiceAClient {
+    @GetMapping("/helloWorld")
+    String helloWorld();
+}
