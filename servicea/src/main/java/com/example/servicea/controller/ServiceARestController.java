589772d81f857d72f7bbbeb0b225f54e2caa797e
qkrwns1478 <qkrwns1478@gmail.com>
2026-05-05 21:51:33 +0900

    feat: config 서버 추가

--- servicea/src/main/java/com/example/servicea/controller/ServiceARestController.java
+++ servicea/src/main/java/com/example/servicea/controller/ServiceARestController.java
@@ -1,16 +1,25 @@
 package com.example.servicea.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope // /actuator/refresh 엔드포인트를 호출하여 설정 변경 사항을 동적으로 반영 가능함
 @RestController
 @RequestMapping("/a")
 public class ServiceARestController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${message}")
+    private String message;
+
 	@GetMapping("/hello")
 	public String helloWorld() {
-		return "Hello world from Service A!";
+		return message + " from Service A " + "Port " + serverPort;
 	}
 
 }
